b61bf7dbdb26b6ff3e76f10173ffb94a76cd4f4e
Guido Lorenz <code@superquadratic.net>
2012-04-28 12:41:18 +0200

    Use numpy array for ROI

--- lego.py
+++ lego.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 
 WINDOW_NAME = 'hello'
@@ -7,18 +8,25 @@ def global_on_mouse(event, x, y, unknown, lego_player):
 
 class LegoPlayer(object):
     def __init__(self):
-        self.rect = []
+        self.rect = np.empty((4, 2))
+        self.rect_index = -1
 
         cv2.namedWindow(WINDOW_NAME)
         cv2.setMouseCallback(WINDOW_NAME, global_on_mouse, self)
         self.capture = cv2.VideoCapture(0)
 
+    def has_roi(self):
+        return self.rect_index == 3
+
     def on_mouse(self, event, x, y):
         if event == cv2.EVENT_LBUTTONDOWN:
-            if len(self.rect) == 4:
-                self.rect = []
-            self.rect.append((x, y))
-            print self.rect
+            if self.has_roi():
+                self.rect_index = -1
+            self.rect_index += 1
+            self.rect[self.rect_index][0] = x
+            self.rect[self.rect_index][1] = y
+            if self.has_roi():
+                print self.rect
 
     def process_frame(self, frame):
         return cv2.split(frame)[2]
