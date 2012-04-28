74ec012c4adc1d00eee41a3d16e89a2a48f38a01
Guido Lorenz <code@superquadratic.net>
2012-04-28 13:37:26 +0200

    Factored out Frame class

--- lego.py
+++ lego.py
@@ -6,51 +6,66 @@ RECT_WINDOW = 'rect'
 CELL_SIZE = 32
 GRID_SIZE = 16 * CELL_SIZE
 
+class Frame(object):
+    def __init__(self, img, homography):
+        self.img = img
+        self.homography = homography
+        cv2.namedWindow(RECT_WINDOW)
+
+    def process(self):
+        warped = cv2.warpPerspective(self.img, self.homography, (GRID_SIZE, GRID_SIZE))
+        cv2.imshow(RECT_WINDOW, warped)
+        #cell = self.get_cell(warped, 0, 0)
+        #cv2.imshow(RECT_WINDOW, cell)
+
+    def get_cell(self, warped, channel, step):
+        cell = warped[
+          channel * CELL_SIZE : (channel + 1) * CELL_SIZE,
+          step * CELL_SIZE : (step + 1) * CELL_SIZE,
+          :]
+        print cell
+
 def global_on_mouse(event, x, y, unknown, lego_player):
     lego_player.on_mouse(event, x, y)
 
 class LegoPlayer(object):
     def __init__(self):
         self.homography = None
-        self.rect = np.empty((4, 2))
-        self.rect_index = -1
+        self.roi = np.empty((4, 2))
+        self.roi_index = -1
 
         cv2.namedWindow(MAIN_WINDOW)
         cv2.setMouseCallback(MAIN_WINDOW, global_on_mouse, self)
-        cv2.namedWindow(RECT_WINDOW)
         self.capture = cv2.VideoCapture(0)
 
     def on_mouse(self, event, x, y):
         if event == cv2.EVENT_LBUTTONDOWN:
             if self.has_roi():
-                self.rect_index = -1
-            self.rect_index += 1
-            self.rect[self.rect_index] = [x, y]
+                self.roi_index = -1
+            self.roi_index += 1
+            self.roi[self.roi_index] = [x, y]
             if self.has_roi():
                 self.compute_homography()
 
     def has_roi(self):
-        return self.rect_index == 3
+        return self.roi_index == 3
 
     def compute_homography(self):
-        src_points = self.rect
+        src_points = self.roi
         dst_points = np.zeros_like(src_points)
         dst_points[1][0] = GRID_SIZE
         dst_points[2] = [GRID_SIZE, GRID_SIZE]
         dst_points[3][1] = GRID_SIZE
         self.homography = cv2.findHomography(src_points, dst_points)[0]
 
-    def process_frame(self, frame):
-        if self.homography is not None:
-            return cv2.warpPerspective(frame, self.homography, (GRID_SIZE, GRID_SIZE))
-
     def loop(self):
         while True:
-            success, frame = self.capture.read()
-            cv2.imshow(MAIN_WINDOW, frame)
-            result = self.process_frame(frame)
-            if result is not None:
-                cv2.imshow(RECT_WINDOW, result)
+            success, img = self.capture.read()
+            if success:
+                cv2.imshow(MAIN_WINDOW, img)
+                if self.homography is not None:
+                    frame = Frame(img, self.homography)
+                    frame.process()
             cv2.waitKey(10)
 
 if __name__ == '__main__':
