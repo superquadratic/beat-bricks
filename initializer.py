5f54a236732ba0d02e0bcbecaea06ae8b983366b
Guido Lorenz <code@superquadratic.net>
2012-05-02 20:02:00 +0200

    Added initializer script
    The initializer shows the camera image. When all four corners of the lego plate
    have been clicked (counter-clockwise, starting at the first step of the first
    channel), it writes a rect.json file containing the plate's location.

---
+++ initializer.py
@@ -0,0 +1,35 @@
+import json
+
+import cv2
+
+WINDOW = 'hello'
+
+class Initializer(object):
+    def __init__(self):
+        self.rect = []
+        self.capture = cv2.VideoCapture(0)
+
+        def on_mouse(event, x, y, unused, user_data):
+            if event == cv2.EVENT_LBUTTONDOWN:
+                self.click(x, y)
+
+        cv2.namedWindow(WINDOW)
+        cv2.setMouseCallback(WINDOW, on_mouse)
+
+    def click(self, x, y):
+        self.rect.append([x, y])
+
+    def run(self):
+        while len(self.rect) < 4:
+            success, frame = self.capture.read()
+            if success:
+                cv2.imshow(WINDOW, frame)
+            if cv2.waitKey(100) != -1:
+                break
+
+
+if __name__ == '__main__':
+    initializer = Initializer()
+    initializer.run()
+    with open('rect.json', 'w') as f:
+        json.dump(initializer.rect, f)
