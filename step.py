cabe559f3eff0e403c93e6d31736280b80f429fd
Guido Lorenz <code@superquadratic.net>
2012-05-02 19:26:14 +0200

    Added step sequencer with solid timing
    The new step sequencer uses PortMidi with latency and timestamps to schedule
    each step ahead of time.

---
+++ step.py
@@ -0,0 +1,41 @@
+import numpy
+import pypm
+
+class Pattern(object):
+    def __init__(self, channels=4, steps=16):
+        self.steps = numpy.ones((steps, channels), bool)
+        self.muted = numpy.zeros(channels, bool)
+
+class StepSequencer(object):
+    def __init__(self, pattern=Pattern()):
+        pypm.Initialize()
+        self.bpm = 120
+        self.pattern = pattern
+        self.output = pypm.Output(pypm.GetDefaultOutputDeviceID(), 50)
+
+    @property
+    def bpm(self):
+        return 15000.0 / self._step_time
+
+    @bpm.setter
+    def bpm(self, bpm):
+        self._step_time = 15000.0 / bpm
+
+    def play(self):
+        next_time = pypm.Time()
+        step = -1
+        while True:
+            if pypm.Time() > next_time:
+                step = (step + 1) % 16
+                next_time += self._step_time
+                self.trigger_step(step, next_time)
+
+    def trigger_step(self, step, timestamp):
+        for channel, note_on in enumerate(self.pattern.steps[step]):
+            if note_on and not self.pattern.muted[channel]:
+                self.output.Write([[[0x90, 36 + channel, 100], timestamp]])
+                self.output.Write([[[0x80, 36 + channel], timestamp]])
+
+if __name__ == '__main__':
+    step = StepSequencer()
+    step.play()
