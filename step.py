d8b2a54cf0dd6709d00e06b9889e4bb221969deb
Guido Lorenz <code@superquadratic.net>
2012-05-05 14:36:04 +0200

    Reduced step sequencer latency

--- step.py
+++ step.py
@@ -3,12 +3,14 @@ import pypm
 
 from pattern import Pattern, PatternListener
 
+LATENCY = 8
+
 class StepSequencer(object):
     def __init__(self, pattern=Pattern()):
         pypm.Initialize()
         self.bpm = 120
         self.pattern = pattern
-        self.output = pypm.Output(pypm.GetDefaultOutputDeviceID(), 50)
+        self.output = pypm.Output(pypm.GetDefaultOutputDeviceID(), LATENCY)
 
     @property
     def bpm(self):
@@ -22,10 +24,12 @@ class StepSequencer(object):
         next_time = pypm.Time()
         step = -1
         while True:
-            if pypm.Time() > next_time:
+            if pypm.Time() >= next_time:
                 step = (step + 1) % 16
-                next_time += self._step_time
                 self.trigger_step(step, next_time)
+                if pypm.Time() - next_time > LATENCY:
+                    print 'WARNING: Inaccurate timing. Increase LATENCY.'
+                next_time += self._step_time
 
     def trigger_step(self, step, timestamp):
         for track, note_on in enumerate(self.pattern.steps[step]):
